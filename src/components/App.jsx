24a637b778f657b4bf21695fc9b103f0d1abb358
taranvd <apple.vlad.taran@gmail.com>
2023-11-09 18:09:25 +0200

    feat: add controller blocks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,15 @@
 import SkySimulation from './SkySimulator/SkySimulator';
 
+const style = {
+  margin: '0 auto',
+  maxWidth: '1200px',
+  textAlign: 'center',
+};
+
 export const App = () => {
   return (
-    <div>
-      <h1>Simulator by TRNVD 🥷</h1>
+    <div style={style}>
+      <h1 style={{ color: 'white' }}>Simulator by TRNVD 🥷</h1>
       <SkySimulation />
     </div>
   );
--- src/components/SkySimulator/SkySimulator.js
+++ src/components/SkySimulator/SkySimulator.js
@@ -1,111 +1,67 @@
 import React, { useState, useEffect } from 'react';
-import { useTrail, useSpring, animated } from 'react-spring';
+import { useTrail, animated } from 'react-spring';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
 import './SkySimulation.css';
 
 const SkySimulation = () => {
-  const [points, setPoints] = useState([]);
   const [isAnimating, setIsAnimating] = useState(false);
-  const [animationSpeed, setAnimationSpeed] = useState(5);
-  const [selectedBlock, setSelectedBlock] = useState(null); // Додано стан для виділеного блоку
+  const [animationSpeed, setAnimationSpeed] = useState(10); // Початкова швидкість
+  const [blockColors, setBlockColors] = useState(Array(12).fill('black'));
 
-  const handleMapClick = e => {
-    if (!isAnimating && selectedBlock) {
-      const x = e.nativeEvent.offsetX;
-      const y = e.nativeEvent.offsetY;
-      setPoints([
-        ...points,
-        { x, y, opacity: 1, color: 'gray', block: selectedBlock },
-      ]);
+  useEffect(() => {
+    if (isAnimating) {
+      const animationInterval = setInterval(() => {
+        setBlockColors(Array(12).fill('white'));
+        setTimeout(() => {
+          setBlockColors(Array(12).fill('black'));
+        }, 100 / animationSpeed);
+      }, 1000 / animationSpeed);
+
+      return () => {
+        clearInterval(animationInterval);
+      };
     }
+  }, [isAnimating, animationSpeed]);
+
+  //* Change slider speed animation
+  const handleSpeedChange = value => {
+    setAnimationSpeed(value);
   };
 
-  const handleClearPoints = () => {
-    // Очищаємо тільки точки, які прив'язані до виділеного блоку
-    setPoints(points.filter(point => point.block !== selectedBlock));
-  };
-
-  const trail = useTrail(points.length, {
-    from: { opacity: 0.3, color: 'gray' },
-    to: { opacity: 1, color: 'white' },
+  const blockColorAnimations = useTrail(12, {
+    from: { backgroundColor: 'grey' },
+    to: {
+      backgroundColor: isAnimating ? 'white' : 'grey',
+    },
     config: { duration: 1000 / animationSpeed },
     reverse: !isAnimating,
   });
 
-  const cometSpring = useSpring({
-    to: isAnimating
-      ? { x: selectedBlock?.x || 0, y: selectedBlock?.y || 0 }
-      : { x: selectedBlock?.x || 0, y: selectedBlock?.y || 0 },
-    config: { duration: 1000 / (animationSpeed * 2) },
-  });
-
-  const handleBlockClick = block => {
-    setSelectedBlock(block);
-  };
-
   return (
     <div className="sky-simulation">
       <button onClick={() => setIsAnimating(!isAnimating)}>
-        {isAnimating ? 'Вимкнути Анімацію' : 'Почати Анімацію'}
+        {isAnimating ? 'off ⏸️' : 'on ▶️'}
       </button>
-      <button onClick={handleClearPoints}>Очистити точки</button>
       <div>
-        <input
-          type="range"
-          min="1"
-          max="20"
-          step="1"
+        <h2>Change speed animation</h2>
+        <Slider
+          min={10}
+          max={50}
           value={animationSpeed}
-          onChange={e => setAnimationSpeed(parseInt(e.target.value))}
+          onChange={handleSpeedChange}
         />
-        <span>Швидкість анімації: {animationSpeed}</span>
       </div>
       <div className="controller">
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((block, index) => (
-          <div
-            key={index}
-            className={`controller-block ${
-              block === selectedBlock ? 'selected' : ''
-            }`} // Виділяємо виділений блок
-            onClick={() => handleBlockClick(block)}
-          >
-            {block}
-          </div>
-        ))}
-      </div>
-      <div className="map" onClick={handleMapClick}>
-        {trail.map((props, index) => (
+        {blockColors.map((color, index) => (
           <animated.div
             key={index}
-            className="point animated"
-            style={{
-              top: points[index].y,
-              left: points[index].x,
-              opacity: props.opacity,
-              backgroundColor: props.color,
-            }}
-          ></animated.div>
+            className="controller-block"
+            style={blockColorAnimations[index]}
+          >
+            {index + 1}
+          </animated.div>
         ))}
-        {isAnimating &&
-          selectedBlock &&
-          points.some(point => point.block === selectedBlock) && (
-            <>
-              <animated.div
-                className="comet"
-                style={{
-                  top: cometSpring.y,
-                  left: cometSpring.x,
-                }}
-              ></animated.div>
-              <animated.div
-                className="comet-trail"
-                style={{
-                  top: cometSpring.y,
-                  left: cometSpring.x,
-                  backgroundColor: 'white',
-                }}
-              ></animated.div>
-            </>
-          )}
       </div>
     </div>
   );
